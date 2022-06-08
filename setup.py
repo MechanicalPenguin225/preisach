0eafa897be6e74abe028fe7b22d2c47671046d36
Lou Bernabeu <bernabeu.lou@gmail.com>
2022-06-08 14:35:35 +0200

    added setup and init files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="preisach",  # Replace with your own username
+    version="0.0",
+    author="Lou Bernabeu",
+    author_email="bernabeu.lou@gmail.com",
+    description="A Python package to numerically implement the Preisach model of hysteresis.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MechanicalPenguin225/preisach",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
