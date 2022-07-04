fa845793a8ba39314ac30bd79e76ce7b86cb0b95
Lou Bernabeu <bernabeu.lou@gmail.com>
2022-07-04 16:24:27 +0200

    sum-based implementation now works, still a lot of imprecisions though

--- preisach.py
+++ preisach.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.integrate import dblquad
+from scipy.interpolate import bisplrep, bisplev
 from .history import History
 
 ### FACTORING OUT HISTORY MANAGEMENT (it's the same for all simple Preisach models)
@@ -42,13 +43,35 @@ class History_primitive():
 
             return delta
 
-# INTEGRAL-BASED IMPLEMENTATION (it slo)
-class Integral_Preisach(History_primitive):
-    def __init__(self, mu, bounds):
 
-        self.mu = mu
-        self.bounds = tuple(bounds)
-        self.history_object = History()
+    def make_flipping_front(self):
+        hist = self.history
+
+        m, M = self.bounds
+
+        if hist is None :
+            return np.array([[m, m],
+                             [m, m]])
+        else :
+            hist_shape = hist.shape
+            corners_shape = (hist_shape[0]*2, hist_shape[1])
+            corners = np.zeros(corners_shape)
+            up_corners = hist
+
+            hist_alpha, hist_beta = hist.T
+            hist_beta = np.roll(hist_beta, 1)
+            hist_beta[0] = m
+            down_corners = np.zeros_like(up_corners)
+            down_corners[:, 0] = hist_alpha
+            down_corners[:, 1] = hist_beta
+
+            corners[::2, :] = down_corners
+            corners[1::2, :] = up_corners
+
+            if corners[-1, 0] != corners[-1, 1]:
+                corners = np.concatenate((corners, np.ones((1, 2))*corners[-1, -1]))
+
+            return corners
 
     def make_integrand_bound_functions(self):
         hist = self.history
@@ -93,6 +116,15 @@ class Integral_Preisach(History_primitive):
 
         return spin_up_max_beta_bound, spin_down_min_alpha_bound
 
+# INTEGRAL-BASED IMPLEMENTATION (it slo)
+class Integral_Preisach(History_primitive):
+    def __init__(self, mu, bounds):
+
+        self.mu = mu
+        self.bounds = tuple(bounds)
+        self.history_object = History()
+
+
     def get_value(self):
         hist = self.history
         bounds = self.bounds
@@ -113,35 +145,6 @@ class Integral_Preisach(History_primitive):
         self.history_object.update(V)
         return self.get_value()
 
-    def make_flipping_front(self):
-        hist = self.history
-
-        m, M = self.bounds
-
-        if hist is None :
-            return np.array([[m, m],
-                             [m, m]])
-        else :
-            hist_shape = hist.shape
-            corners_shape = (hist_shape[0]*2, hist_shape[1])
-            corners = np.zeros(corners_shape)
-            up_corners = hist
-
-            hist_alpha, hist_beta = hist.T
-            hist_beta = np.roll(hist_beta, 1)
-            hist_beta[0] = m
-            down_corners = np.zeros_like(up_corners)
-            down_corners[:, 0] = hist_alpha
-            down_corners[:, 1] = hist_beta
-
-            corners[::2, :] = down_corners
-            corners[1::2, :] = up_corners
-
-            if corners[-1, 0] != corners[-1, 1]:
-                corners = np.concatenate((corners, np.ones((1, 2))*corners[-1, -1]))
-
-            return corners
-
 # SUM-BASED IMPLEMENTATION (it fast but it don't work)
 class Preisach(History_primitive):
     def __init__(self, preisach_coords, measured_preisach_mesh, strict_f = False):
@@ -153,25 +156,35 @@ class Preisach(History_primitive):
         self.preisach_coords = preisach_coords
         self.offset = offset
 
+        self.bounds = (np.min(preisach_coords), np.max(preisach_coords))
+
         self.f = self.generate_model(preisach_coords, measured_preisach_mesh, strict_f)
         self.history_object = History() # this will be a n by 2 array . history[i, :] = [M_i, m_i]
-        self.f_plus = self.f(self.model_bounds[-1], self.model_bounds[-1])
+        self.f_plus = self.f(self.bounds[-1], self.bounds[-1])
 
-    def generate_model(self, preisach_coords, measured_preisach_mesh, strict_f):
+    def generate_model(self, preisach_coords, measured_preisach_mesh, strict_f = True):
         """Interpolates the $f_{\alpha, \beta}$ function from measurements and returns it."""
         alpha_values = preisach_coords[:, 0]
         beta_values = preisach_coords[:, 1]
 
         min_u = np.min(alpha_values)
         max_u = np.max(alpha_values)
-        self.model_bounds = np.array([min_u, max_u])
+        self.model_bounds = self.bounds
 
-        interpolated_mesh = interp2d(alpha_values, beta_values, measured_preisach_mesh, kind = "cubic")
+        spline_representation = bisplrep(alpha_values, beta_values, measured_preisach_mesh, kx = 5, ky = 5)
+        def interpolated_mesh(x, y):
+            if x > max_u :
+                x = max_u
+
+            if y < min_u :
+                y = min_u
+
+            return bisplev(x, y, spline_representation)
 
         if strict_f :
             def mesh(alpha, beta):
-                if alpha < beta or not(np.all(min_u <= np.array([alpha, beta]))) or not(np.all(max_u >= np.array([alpha, beta]))):
-                    raise ValueError("Preisach function called outside Preisach triangle.")
+                if alpha < beta :
+                    raise ValueError(f"Preisach function called outside Preisach triangle : {alpha}, {beta}")
                 else :
                     return interpolated_mesh(alpha, beta)
         else :
@@ -183,7 +196,7 @@ class Preisach(History_primitive):
 
     def get_value(self, **kwargs):
 
-        f = self.f
+        f_func = self.f
 
         if "history" in kwargs.keys():
             hist = kwargs["history"]
@@ -191,11 +204,11 @@ class Preisach(History_primitive):
             hist = self.history
 
         max_values = hist[:, 0] # the list of M_k values
-        mins_k = hist[:, 0] # the list of m_k values
+        mins_k = hist[:, 1] # the list of m_k values
         mins_km1 = np.roll(mins_k, 1) # roll it, and then
         mins_km1[0] = self.model_bounds[0] # ... set a value for m_0 to get the list of k_{-1}
 
-        terms = f(max_values, mins_k) - f(max_values, mins_km1)
+        terms = f_func(max_values, mins_k) - f_func(max_values, mins_km1)
 
         return self.offset - self.f_plus + np.sum(terms)
 
